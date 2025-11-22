4ec7e90b28bd948130254cdac2b522066449c456
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-11-21 19:39:15 -0500

    Add dotenv for environment config and update OpenAI usage
    Introduced dotenv to manage environment variables and updated the OpenAI client usage in iaConnection.js. The request to OpenAI now includes additional parameters for reasoning, instructions, and disables response storage. Updated dependencies in package.json.

--- iaConnection.js
+++ iaConnection.js
@@ -1,9 +1,17 @@
-import OpenAI from "openai";
-const client = new OpenAI();
+import dotenv from "dotenv";
+dotenv.config();
 
-const response = await client.responses.create({
+
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const response = await openai.responses.create({
     model: "gpt-5-nano",
-    input: "Write a one-sentence bedtime story about a unicorn."
+    reasoning: {effort: "low"},
+    instructions: "Eres unamini-aplicación de IA llamada 'IdeaBoost', que ayuda a los usuarios a generar ideas creativas, y respondes una sola vez, no le haces preguntas al usuario.",
+    input: "dame una idea para un proyecto escolar innovador.",
+    store: false,
 });
 
 console.log(response.output_text);
+
